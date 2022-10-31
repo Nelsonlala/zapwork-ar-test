c7a237a88446ec15acbdc8389c16ae65cf170b07
Nelsonlala <106651544+Nelsonlala@users.noreply.github.com>
2022-10-31 16:05:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,42 @@ function setup() {
 
     const myImageGroup = document.getElementById("image-group");
 
+       const portal = document.querySelector('#portal')
+     const sky = document.querySelector('#background-img')
+      sky.setAttribute('src', '#field2')
+          let    ball = document.querySelector('#ball')
+          
     let imageVisible = false;
 
     myImageGroup.addEventListener("zappar-visible", () => {
         // The image has appeared so show the group
         myImageGroup.setAttribute("visible", "true");
         imageVisible = true;
+        splatSnd.components.sound.playSound()
+        
+          setTimeout(() => {
+          portal.setAttribute('animation__scaleIn', {
+            property: 'radius-inner',
+            dur: 1500,
+            from: '0.001',
+            to: '0.375',
+            easing: 'easeOutElastic',
+          })
+        }, 200)
+
+        // ball animation
+        setTimeout(() => {
+        // splatSnd.playSound()
+          ball.setAttribute('animation__moveOut', {
+          // model.setAttribute('animation__moveOut', {
+            property: 'position',
+            dur: 3000,
+            from: '0 0 -10',
+            to: '0 -0.2 0.6',
+            easing: 'easeOutQuad',
+          })
+  }, 1000)
+       
     });
 
     myImageGroup.addEventListener("zappar-notvisible", () => {
@@ -20,6 +50,7 @@ function setup() {
         imageVisible = false;
         setTimeout(() => {
             if (imageVisible === false) myImageGroup.setAttribute("visible", "false");
+             splatSnd.components.sound.stopSound()
         }, 500)
     });
 
